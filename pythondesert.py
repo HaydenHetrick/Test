2e53dfdb8d11f33db92ae95e5fe63a099a74cc46
HaydenHetrick <143512342+HaydenHetrick@users.noreply.github.com>
2023-09-07 09:58:05 -0400

    Update pythondesert.py

--- pythondesert.py
+++ pythondesert.py
@@ -2,9 +2,10 @@ def ask_dessert():
   dessert = input("Would you like the Dessert Menu? ")
   if dessert == "Yes" :
     print("Very well here is the dessert menu. ")
-    print("Rose Water Rice Pudding, Tres Leches Cake, Kulfi, Bread Pudding, Beighets,")
+    print("Rose Water Rice Pudding, Tres Leches Cake, Kulfi, Bread Pudding, Beignets, ")
     dessert_choose = input("What dessert would you like? ")
-    print(f"Great choice I will have the {dessert_choose} out right away") 
+    print(f"Great choice I will have the {dessert_choose} out right away ") 
   elif dessert == "No" :
-    print("That's no problem here is the bill. ")
+    print("That's no problem here is the bill sir. ")
+    
 ask_dessert()
